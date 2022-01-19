40f6897990d8735da712743170322ea9fe86f7c4
Shernan Javier <shernanjavier@gmail.com>
2022-01-19 02:48:45 -0500

    Created a new gallery component to separate from index file

--- pages/index.js
+++ pages/index.js
@@ -2,60 +2,9 @@ import React from "react";
 import Head from "next/head";
 import APOD from "../components/APOD/APOD";
 import axios from "axios";
-import styled from "styled-components";
-import Image from "next/image";
-export const Cover = styled.section`
-  min-height: 100vh;
-  width: 100%;
-  background-color: #fbf7ed;
-  z-index: 2;
-`;
+import Gallery from "../components/Gallery/Gallery";
 
-const Home = ({ data: apod, today }) => {
-  let posts = apod;
-
-  const gallery = posts.map((post, i) => {
-    let url = post.url;
-
-    // Checks media type, if it is a video, it uses the thumbnail url.
-    if (post.media_type.includes("video")) {
-      url = post.thumbnail_url;
-      // Some urls contain html instead of an image, causes site to crash and has no thumbnail...
-      // therefore having to exclude it from the gallery.
-      if (post.url.includes(".html")) {
-        return null;
-      }
-    }
-
-    return (
-      <div key={i}>
-        <div
-          style={{
-            position: "relative",
-            width: "auto",
-            height: "30rem",
-            maxWidth: "30rem",
-            margin: "20px",
-            backgroundColor: "lightcoral",
-          }}
-        >
-          <Image
-            src={url}
-            alt={post.title}
-            layout="fill"
-            objectFit="cover"
-            loading="lazy"
-          />
-        </div>
-        <span>{post.date}</span>
-        <h1>{post.title}</h1>
-        <h2>{post.copyright}</h2>
-        <p>{post.explanation}</p>
-        <button>Like</button>
-      </div>
-    );
-  });
-
+const Home = ({ gallery, today }) => {
   return (
     <>
       <Head>
@@ -64,18 +13,30 @@ const Home = ({ data: apod, today }) => {
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
-      <nav>
-        <ul>
-          <li>Spacestagram</li>
+      <nav
+        style={{
+          position: "fixed",
+          top: "0",
+          left: "0",
+          zIndex: "99",
+          backgroundColor: "#fbf7ed",
+          backgroundImage: "radial-gradient(gray 0.3px, #fbf7ed 0.5px)",
+          backgroundSize: "20px 20px",
+          backgroundPosition: "0 0, 10px 10px",
+          height: "60px",
+          width: "100%",
+          boxShadow:
+            "0px 3px 5px -1px rgb(0 0 0 / 20%), 0px 5px 8px 0px rgb(0 0 0 / 14%), 0px 1px 14px 0px rgb(0 0 0 / 12%)",
+          // borderBottom: "1px solid black",
+        }}
+      >
+        <ul style={{ listStyle: "none" }}>
+          <li style={{ fontSize: "24px", fontWeight: "bold" }}>Spacestagram</li>
         </ul>
       </nav>
       <main>
         <APOD data={today} />
-        <Cover>
-          <div style={{ display: "flex", flexDirection: "column" }}>
-            {gallery}
-          </div>
-        </Cover>
+        <Gallery data={gallery} />
       </main>
     </>
   );
@@ -87,18 +48,18 @@ export const getServerSideProps = async (context) => {
   const today = await axios.get(
     `https://api.nasa.gov/planetary/apod?api_key=${process.env.API_KEY}`
   );
-  // const res = await axios.get(
-  //   `https://api.nasa.gov/planetary/apod?api_key=${process.env.API_KEY}&count=${
-  //     !isNaN(count) ? count : 10
-  //   }&thumbs=true`
-  // );
-  //Used to test date ranges
   const res = await axios.get(
-    `https://api.nasa.gov/planetary/apod?api_key=${process.env.API_KEY}&start_date=2021-08-01&end_date=2021-08-18&thumbs=true`
+    `https://api.nasa.gov/planetary/apod?api_key=${process.env.API_KEY}&count=${
+      !isNaN(count) ? (count <= 50 && count > 0 ? count : 10) : 10
+    }&thumbs=true`
   );
+  //Used to test date ranges
+  // const res = await axios.get(
+  //   `https://api.nasa.gov/planetary/apod?api_key=${process.env.API_KEY}&start_date=2021-08-01&end_date=2021-08-18&thumbs=true`
+  // );
 
   return {
-    props: { data: res.data, today: today.data },
+    props: { gallery: res.data, today: today.data },
   };
 };
 export default Home;
